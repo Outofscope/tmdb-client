c29634a60cd537d3010b20ce9713de0ca8206b2e
Konstantin Medvedenko <medvedenko@gmail.com>
2020-11-28 13:43:50 +0200

    Initial Commit

---
+++ Movies/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Konstantin Medvedenko on 28.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
